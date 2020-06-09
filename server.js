e1b2d8bd31cbd742158409a6fdc06735fb78dac2
Marco <4high20@gmail.com>
2020-06-09 07:47:08 +0100

    Add MongoDB Connection

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+require('dotenv').config()
 
 const PORT = process.env.PORT || 5000;
+
+// Connect To Mongodb
+mongoose.connect(process.env.MONGODB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
 
 app.use(express.static(path.join(__dirname, './client/build')))
 
