50f055fa4e1e322cb241fa676579e251ceb63aed
Jon Hill <jondhill1984@gmail.com>
2020-06-12 17:32:59 +0300

    Add component and structure for calender display

--- client/src/App.js
+++ client/src/App.js
@@ -2,30 +2,9 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import CalenderDisplay from './components/calenderDisplay/calenderDisplay';
+
 function App() {
-
-let today = new Date();
-let month = new Array();
-
-month[0] = "Jan";
-month[1] = "Feb";
-month[2] = "March";
-month[3] = "April";
-month[4] = "May";
-month[5] = "June";
-month[6] = "July";
-month[7] = "Aug";
-month[8] = "Sept";
-month[9] = "Oct";
-month[10] = "Nov";
-month[11] = "Dec";
-
-
-let currentMonth = month[today.getMonth()];
-let prevMonth = month[today.getMonth() - 1];
-let nextMonth = month[today.getMonth() + 1];
-let currentYear = today.getFullYear();
-
 
   return (
     <div className="app">
@@ -35,47 +14,7 @@ let currentYear = today.getFullYear();
         </div>
       </div>
       <div className="mainContainer">
-        <div className="calenderDisplay">
-          <div className="currrentYear">{currentYear}</div>
-          <div className="monthsDisplay">
-            <div>{prevMonth}</div>
-            <div><strong>{currentMonth}</strong></div>
-            <div>{nextMonth}</div>
-          </div>
-          <div className="daysDisplay">
-            <div>Mon</div>
-            <div>Tues</div>
-            <div>Weds</div>
-            <div>Thurs</div>
-            <div>Fri</div>
-            <div>Sat</div>  
-            <div>Sun</div>           
-            <div className="day"></div>
-            <div className="day"></div>
-            <div className="day"></div>
-            <div className="day"></div>
-            <div className="day"></div>
-            <div className="day"></div>
-            <div className="day"></div>
-            <div className="day"></div>
-            <div className="day"></div>
-            <div className="day"></div>
-            <div className="day"></div>
-            <div className="day"></div>
-            <div className="day"></div>
-            <div className="day"></div>
-            <div className="day"></div>
-            <div className="day"></div>
-            <div className="day"></div>
-            <div className="day"></div>
-            <div className="day"></div>
-            <div className="day"></div>
-            <div className="day"></div>
-            <div className="day"></div>
-            <div className="day"></div>
-            <div className="day"></div>
-          </div>
-        </div>
+        <CalenderDisplay/>
         <div className="eventsDisplay"></div>
         </div>
         <div className="footer">
@@ -84,5 +23,4 @@ let currentYear = today.getFullYear();
     </div>
   );
 }
-
 export default App;
