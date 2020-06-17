621e1d296cbd83c432ed66f245b14688299315b6
Marco <4high20@gmail.com>
2020-06-17 10:14:45 +0100

    Add interactive calendar feature, need refactoring

--- client/src/components/CalenderDisplay/calenderDisplay.js
+++ client/src/components/CalenderDisplay/calenderDisplay.js
@@ -1,70 +1,114 @@
-import React from 'react';
-import './calenderDisplay.styles.css';
+import React from "react";
+import "./calenderDisplay.styles.css";
 
+let today = new Date();
+let currentMonth = today.getMonth();
+let currentYear = today.getFullYear();
 
-function CalenderDisplay() {
+const months = [
+  "Jan",
+  "Feb",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "Aug",
+  "Sept",
+  "Oct",
+  "Nov",
+  "Dec",
+];
 
-    let today = new Date();
-    let currentMonth = today.getMonth();
+let days = [];
 
-    const months = [
-      { key: 0, name: "Jan", days: 31, startday: 3 }, { key: 1, name: "Feb", days: 28, startday: 6 }, { key: 2, name: "March", days: 31, startday: 0},
-      { key: 3, name: "April", days: 30, startday: 3}, { key: 4,name: "May",days: 31, startday: 5}, { key: 5,name: "June", days: 30, startday: 1},
-      { key: 6, name: "July", days: 31, startday: 3}, { key: 7, name: "Aug", days: 31, startday: 6 }, { key: 8, name: "Sept", days: 30, startday: 2},
-      { key: 9, name: "Oct", days: 30, startday: 4 }, { key: 10, name: "Nov", days: 30, startday: 0 }, { key: 11, name: "Dec", days: 31, startday: 2}
-    ];
+let prevMonth = months[currentMonth - 1];
+let nextMonth = months[currentMonth + 1];
 
-    let days = [];
+class CalenderDisplay extends React.Component {
+  render() {
+    let currentMonthDisplay = months[currentMonth];
 
-    let currentMonthDisplay = months[currentMonth].name;
-    let prevMonth = months[currentMonth - 1].name;
-    let nextMonth = months[currentMonth + 1].name;
-    let currentYear = today.getFullYear();
+    let startDayOfWeek = new Date(currentYear, currentMonth).getDay();
+    let numOfDays = 32 - new Date(currentYear, currentMonth, 32).getDate();
+    for (let i = 1; i <= numOfDays; i++) {
+      days.push(i);
+    }
 
-    let startDayOfWeek = months[currentMonth].startday;
-    
-    (function createDays() {
-        months.map(month => {
-            if(month.key === currentMonth) {
-                for(let i = 0; i <= month.days; i++) {
-                    days.push(i);
-                } 
-            }
-        });
-    })();
+    const daysToDisplay = [];
 
-    const daysToDisplay = days.map(day =>
-        {
-            if (day < startDayOfWeek) {
-                return <div className="day"></div>
-            } else {
-                return <div className="day">{day}</div>
-            }
-        }
-    );
+    for (let i = 0; i < 42; i++) {
+      if (days[i] <= startDayOfWeek) {
+        daysToDisplay.push(<div className="day"></div>);
+      } else {
+        daysToDisplay.push(<div className="day">{days.shift()}</div>);
+      }
+    }
 
+    const prevBtn = () => {
+      if (currentMonth === 1) {
+        currentMonth--;
+        prevMonth = "Dec";
+        nextMonth = months[currentMonth + 1];
+        this.forceUpdate();
+      } else if (currentMonth === 0) {
+        currentYear--;
+        currentMonth = 11;
+        prevMonth = months[currentMonth - 1];
+        nextMonth = "Jan";
+        this.forceUpdate();
+      } else {
+        currentMonth--;
+        prevMonth = months[currentMonth - 1];
+        nextMonth = months[currentMonth + 1];
+        this.forceUpdate();
+      }
+    };
+    const nextBtn = () => {
+      if (currentMonth === 10) {
+        currentMonth++;
+        nextMonth = "Jan";
+        prevMonth = months[currentMonth - 1];
+        this.forceUpdate();
+      } else if (currentMonth === 11) {
+        currentYear++;
+        currentMonth = 0;
+        prevMonth = "Dec";
+        nextMonth = months[currentMonth + 1];
+        this.forceUpdate();
+      } else {
+        currentMonth++;
+        prevMonth = months[currentMonth - 1];
+        nextMonth = months[currentMonth + 1];
+        this.forceUpdate();
+      }
+    };
+    console.log(currentMonth);
+    console.log(prevMonth);
+    console.log(nextMonth);
     return (
-        <div className="calenderDisplay">
-            <div className="currrentYear">{currentYear}</div>
-            <div className="monthsDisplay">
-                <div>{prevMonth}</div>
-                <div>{currentMonthDisplay}</div>
-                <div>{nextMonth}</div>
-            </div>
-            <div className="daysOfWeekDisplay">
-                <div>Sun</div> 
-                <div>Mon</div>
-                <div>Tues</div>
-                <div>Weds</div>
-                <div>Thurs</div>
-                <div>Fri</div>
-                <div>Sat</div>  
-                {daysToDisplay}
-
-            </div>
+      <div className="calenderDisplay">
+        <div className="currrentYear">{currentYear}</div>
+        <div className="monthsDisplay">
+          <div>{prevMonth}</div>
+          <div>{currentMonthDisplay}</div>
+          <div>{nextMonth}</div>
         </div>
-
-      );
-}    
+        <div className="daysOfWeekDisplay">
+          <div>Sun</div>
+          <div>Mon</div>
+          <div>Tues</div>
+          <div>Weds</div>
+          <div>Thurs</div>
+          <div>Fri</div>
+          <div>Sat</div>
+          {daysToDisplay}
+        </div>
+        <button onClick={prevBtn}>prev</button>
+        <button onClick={nextBtn}>next</button>
+      </div>
+    );
+  }
+}
 
 export default CalenderDisplay;
